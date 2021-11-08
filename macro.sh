ea22ec1b63845c835268decfa8106758340a21c0
simoncreek <simoncreek@tutanota.com>
2021-11-08 21:01:36 +0900

    New ci setup

--- macro.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-m4 -I template template/index.html > src/index.html
-m4 -I template template/aboutme.html > src/aboutme.html
-m4 -I template template/portfolio_kr.html > src/portfolio_kr.html
